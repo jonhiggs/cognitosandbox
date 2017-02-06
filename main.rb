742ef0a81a2a35163187616123cf08a80b9d0e45
Jon Higgs <jhiggs@eml.cc>
2017-02-06 15:06:26 +1100

    Sign up a user

--- main.rb
+++ main.rb
@@ -14,6 +14,49 @@ def list_users
     Client.list_users({user_pool_id: "us-east-1_ppS2C0w2h"})
 end
 
+def sign_up(email,password,field_size=1)
+  data = (0...field_size).map { ('a'..'z').to_a[rand(26)] }.join
+  Client.sign_up({
+    client_id: CLIENT_ID,
+    username: email,
+    password: password,
+    user_attributes: [
+      {
+        name: 'email',
+        value: email
+      },
+      {
+        name: 'data_01',
+        value: data
+      },
+      {
+        name: 'data_02',
+        value: data
+      },
+      {
+        name: 'data_03',
+        value: data
+      },
+      {
+        name: 'data_04',
+        value: data
+      },
+      {
+        name: 'data_05',
+        value: data
+      },
+      {
+        name: 'data_06',
+        value: data
+      },
+      {
+        name: 'data_07',
+        value: data
+      },
+    ]
+  })
+end
+
 def auth(username, password)
     Client.admin_initiate_auth({
         user_pool_id: "us-east-1_ppS2C0w2h",
@@ -27,20 +70,14 @@ def auth(username, password)
 end
 
 token = auth('erica', 'fish1234')
-#require 'byebug'
-#byebug
+random_string = (0...50).map { ('a'..'z').to_a[rand(26)] }.join
+email = "#{random_string}@rb.com"
+password = random_string
+sign_up(email, password, 1)
+
+puts "signing up user '#{email}'."
+
 authentication_result = token.authentication_result.to_hash
 
-access_token = authentication_result[:access_token]
 id_token = authentication_result[:id_token]
-refresh_token = authentication_result[:refresh_token]
-
-puts "access token:\t#{access_token.size}"
 puts "id token:\t#{id_token.size}"
-puts "refresh token:\t#{refresh_token.size}"
-puts "total size:\t#{access_token.size + id_token.size + refresh_token.size}"
-
-
-
-
-#p token.size
